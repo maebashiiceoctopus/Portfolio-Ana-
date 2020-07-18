749dece8ad7d3ebe5a580dbe7afa0797d8a94761
Alejandra Acosta <macostaq94@gmail.com>
2020-07-18 19:17:59 +0200

    img
    se agregan imagenes para la galeria

---
+++ js/main.js
@@ -0,0 +1,11 @@
+const grid = new Muuri('.grid',¨{
+    layout:{
+        fillGaps: false,
+        horizontal: false,
+        alignRight: false,
+        alignBottom: false,
+        rounding: false
+
+    }
+}
+ );
